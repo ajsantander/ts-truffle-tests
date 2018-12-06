f5cb0b329ca015561e57d3e22d6e4247dbedbd12
Alejandro Santander <Palebluedot@gmail.com>
2018-12-06 15:14:32 -0300

    Truffle tests working with Typescript ;D

--- test/test.js
+++ test/test.js
@@ -1,11 +1,5 @@
-/* 
- * NOTE: Actual contracts and web3 stuff is disabled (commented out).
- * The point of this is to show that mocha tests can resolve both
- * es6 syntax and imported Typescript files.
- */
-
 var assert = require('assert');
-// const Contract = artifacts.require("Contract");
+const Contract = artifacts.require("Contract");
 
 import jsutil from '../src/jsutil.js';
 import tsutil from '../src/tsutil.ts';
@@ -14,14 +8,13 @@ describe("Contract", function(accounts) {
 
   it("Should return Hello", async function() {
 
-    // const contract = await Contract.new();
-    // const contract = getTruffleContract(Contract);
+    const contract = await Contract.new();
 
     await jsutil(500);
     await tsutil(500);
 
-    // const str = await contract.test();
-    // assert.equal(str, "Hello");
+    const str = await contract.test();
+    assert.equal(str, "Hello");
 
     assert.equal([1, 2, 3].indexOf(4), -1);
 
